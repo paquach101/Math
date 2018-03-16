90b0cf5a66a087aafbc34861002ce608d37246e0
Pat the Destroyer213 <phatquach@Pats-iMac.local>
2018-03-16 00:47:11 -0700

    dsa

--- math.js
+++ math.js
@@ -7,16 +7,20 @@ function coefficients() {
     var a = document.getElementById('a').value,
         b = document.getElementById('b').value,
         c = document.getElementById('c').value,
-        radical = (Math.sqrt((b * b) - (4 * a * c))),
+        //discriminant
+        dis = ((b * b) - (4 * a * c)),
         bottom = (2 * a),
-        B = (-1 * b),
-        quadratic1 = ((B - radical) / bottom),
-        quadratic2 = ((B + radical) / bottom);
+        quadratic1 = (((-b) - Math.sqrt(dis)) / bottom),
+        quadratic2 = (((-b) + Math.sqrt(dis)) / bottom);
+    // var AC = 4 * a * c,
+    ///  BB = b * b,
+    //roots = BB - AC;
     if (isNaN(a) || isNaN(b) || isNaN(c) == true) {
-        return "You have entered something wrong. Plz try again."
-    } else if ((b * b) ) {
-
+        return "You entered an incorrect value for either A, B, or C. Please try again."
+    } else if ((dis < 0)) {
+        return "There are no real solutions."
     } else {
         return "The answer is " + quadratic1 + " and " + quadratic2;
+
     }
 }
